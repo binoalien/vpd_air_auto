d0b4717ed093642e6d12fe215ab96388e48b2710
binoalien <binoalien@gmx.de>
2026-05-05 15:21:03 +0200

    Fix pytest fixture injection for custom integrations

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,6 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def auto_enable_custom_integrations(_enable_custom_integrations: None) -> None:
+def auto_enable_custom_integrations(enable_custom_integrations: None) -> None:
     """Enable loading integrations from ``custom_components`` for all tests."""
+    _ = enable_custom_integrations
